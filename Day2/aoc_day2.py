23fc8d24213b061ab2c018de4b181199cb7b164e
Ian Lewis <ian.r.lewis@bt.com>
2023-12-02 10:46:40 +0000

    Solved Day2

--- Day2/aoc_day2.py
+++ Day2/aoc_day2.py
@@ -6,7 +6,7 @@ https://adventofcode.com/2023/day/2
 
 from time import perf_counter
 
-TEST = True
+TEST = False
 
 DAY = "2"
 REAL_INPUT = "Advent-of-Code-2023/Day" + DAY + "/input_day" + DAY + ".txt"
@@ -16,6 +16,12 @@ if TEST:
     FILENAME = TEST_INPUT
 else:
     FILENAME = REAL_INPUT
+
+COLOUR_LIMITS = {
+    "red": 12,
+    "green": 13,
+    "blue": 14,
+}
 
 
 def get_input_data(filename):
@@ -30,6 +36,48 @@ def get_input_data(filename):
 def main():
     """Main program"""
     data = get_input_data(FILENAME)
+
+    game_total = 0
+    for game in data:
+        game_possible = True
+        least_colours = {
+            "red": 1,
+            "green": 1,
+            "blue": 1,
+        }
+        game_number = int(game.split(":")[0][5:])
+        game_subsets = game.split(":")[1].split(";")
+        for subset in game_subsets:
+            cube_results = subset.split(",")
+            for result in cube_results:
+                number_of_cubes, colour_of_cubes = result.split()
+                if int(number_of_cubes) > COLOUR_LIMITS[colour_of_cubes]:
+                    game_possible = False
+                    break
+        if game_possible:
+            game_total += game_number
+    print(f"Part I - Game Total: {game_total}")
+
+    game_total = 0
+    for game in data:
+        least_colours = {
+            "red": 1,
+            "green": 1,
+            "blue": 1,
+        }
+        game_number = int(game.split(":")[0][5:])
+        game_subsets = game.split(":")[1].split(";")
+        for subset in game_subsets:
+            cube_results = subset.split(",")
+            for result in cube_results:
+                number_of_cubes, colour_of_cubes = result.split()
+                if int(number_of_cubes) > least_colours[colour_of_cubes]:
+                    least_colours[colour_of_cubes] = int(number_of_cubes)
+        game_power = (
+            least_colours["red"] * least_colours["blue"] * least_colours["green"]
+        )
+        game_total += game_power
+    print(f"Part II - Game Total: {game_total}")
 
 
 if __name__ == "__main__":

5f84d326c55f1694f3dd5ac29afc5dd348728f14
Ian Lewis <ian.r.lewis@bt.com>
2024-01-15 11:19:24 +0000

    Still working on part II

--- Day10/aoc_day10.py
+++ Day10/aoc_day10.py
@@ -8,7 +8,7 @@ import math
 from time import perf_counter
 import pprint
 
-TEST = True
+TEST = False
 
 DAY = "10"
 REAL_INPUT = "Advent-of-Code-2023/Day" + DAY + "/input_day" + DAY + ".txt"
@@ -70,6 +70,7 @@ def main():
     print(f"Part II - Tiles within the pipe {len(inside_points)}")
     # Part II 397 is too low and 408 is too high
     # even after adding the extra turn points !!! So where is it going wrong? Unless I have it wrong...
+    # My test proves the extra TURN_FWD_ADJ_POINTS does work, but this does change my result for me reald data
 
 
 def trace_pipe(pipe_map, start):
@@ -89,20 +90,20 @@ def trace_pipe(pipe_map, start):
         previous_direction = direction
         direction, change_y, change_x = PIPES[symbol][direction]
         right_turn_count += TURNS[previous_direction][direction]
-        # if right_turn_count > 0:
-        #    lhs_points.add(
-        #        (
-        #            y + TURN_FWD_ADJ_POINTS[direction][0],
-        #            x + TURN_FWD_ADJ_POINTS[direction][1],
-        #        )
-        #    )
-        # elif right_turn_count < 0:
-        #    rhs_points.add(
-        #        (
-        #            y + TURN_FWD_ADJ_POINTS[direction][0],
-        #            x + TURN_FWD_ADJ_POINTS[direction][1],
-        #        )
-        #    )
+        if right_turn_count > 0:
+            lhs_points.add(
+                (
+                    y + TURN_FWD_ADJ_POINTS[direction][0],
+                    x + TURN_FWD_ADJ_POINTS[direction][1],
+                )
+            )
+        elif right_turn_count < 0:
+            rhs_points.add(
+                (
+                    y + TURN_FWD_ADJ_POINTS[direction][0],
+                    x + TURN_FWD_ADJ_POINTS[direction][1],
+                )
+            )
         y += change_y
         x += change_x
         symbol = pipe_map[y][x]

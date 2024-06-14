e60ba9466112d867f87a6b89c9acc7b404dc1fd5
Ian Lewis <ian.r.lewis@bt.com>
2024-06-14 13:07:18 +0100

    tidied up aco12

--- Day12/aoc_day12.py
+++ Day12/aoc_day12.py
@@ -7,7 +7,7 @@ https://adventofcode.com/2023/day/12
 from time import perf_counter
 import sys
 
-TEST = False
+TEST = True
 
 DAY = "12"
 REAL_INPUT = "Advent-of-Code-2023/Day" + DAY + "/input_day" + DAY + ".txt"
@@ -28,14 +28,6 @@ def main():
     data = get_input_data(FILENAME)
     calculate(data)
 
-    # Odd, but if I pass in a list rather than a tuple to permute_string I get a wrong answer in some cases
-    # try permute_string("????.#...#...", [4, 1, 1]) gives result 0 rather than 1
-    # print(f'{permute_string("????.#...#...", [4, 1, 1])}')
-    # print(f'{permute_string("????.#...#...", (4, 1, 1))}')
-
-    # Using permute_string without Memoisation works for part I. but with Memoisation it doesn't work
-    # short_perm_string gives the wrong result for part I
-
 
 def calculate(data):
     """Calculate the total number of variations"""
@@ -51,22 +43,6 @@ def calculate(data):
         )
         # print(f"{spring_string} {damaged_data} Running Count = {count}")
     print(f"Count = {count}")
-
-
-def compare(data):
-    """Output the comparative counts of the two permute functions"""
-    print(f"String\tData\tShort_Perm\tPermute")
-    for line in data:
-        spring_string, damaged_data = line.split(" ")
-        damaged_data = damaged_data.split(",")
-        damaged_data = tuple([int(x) for x in damaged_data])
-        spring_string = grow_string(spring_string, MULTIPLIER)
-        damaged_data = damaged_data * MULTIPLIER
-        short = short_perm_string(spring_string, damaged_data)
-        long = permute_string(spring_string, damaged_data)
-        if short != long:
-            print(f"{spring_string}\t{damaged_data}\t{short}\t{long}")
-            sys.exit()
 
 
 def permute_string(input_string, damaged_data, count=0, memoize=False):
@@ -112,30 +88,6 @@ def permute_string(input_string, damaged_data, count=0, memoize=False):
     return count
 
 
-def short_perm_string(input_string, damaged_data, count=0):
-    """Recursively calculate the number of perumtations of the given string and damaged spring data"""
-    next_qm = input_string.find("?")
-    if next_qm > -1:
-        first_section = input_string[:next_qm]
-        second_section = input_string[next_qm + 1 :]
-        first_section_damaged_data = calculate_damaged_spring_data(first_section)
-        remaining_damaged_data = subtract_sequences(
-            damaged_data, first_section_damaged_data
-        )
-        qm_credit = (
-            input_string.count("?") - sum(damaged_data) + input_string.count("#")
-        )
-        if match_sequences(first_section_damaged_data, damaged_data) and qm_credit >= 0:
-            count = short_perm_string(second_section, remaining_damaged_data, count)
-        if qm_credit >= 0:
-            new_string = first_section + "#" + second_section
-            count = short_perm_string(new_string, damaged_data, count)
-    else:
-        if calculate_damaged_spring_data(input_string) == damaged_data:
-            count += 1
-    return count
-
-
 def calculate_damaged_spring_data(spring_string):
     """Return a tuple of the damaged spring data based on the spring_string"""
     count = 0
@@ -174,20 +126,6 @@ def grow_string(input_string, n, separator="?"):
     for _ in range(n - 1):
         output_string += separator + input_string
     return output_string
-
-
-def reduce_dots(input_string):
-    """Redcue consecutive sequences of '.' to a single '.' and return the string"""
-    ouput_string = ""
-    found_dot = False
-    for c in input_string:
-        if c == "." and found_dot == False:
-            found_dot = True
-            ouput_string += c
-        elif c != ".":
-            found_dot = False
-            ouput_string += c
-    return ouput_string
 
 
 def get_input_data(filename):
